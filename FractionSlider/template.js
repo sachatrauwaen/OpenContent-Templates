342b83b128cfff9c33ec865d2467fa8510eeeefc
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2015-06-21 14:19:06 +0200

    fraction slider

---
+++ FractionSlider/template.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $(".fraction-slider").each(function () {
+        $(this).fractionSlider({
+            'fullWidth': $(this).attr("data-fullWidth") ? $(this).data("fullWidth") : false,
+            'controls': $(this).attr("data-controls") ? $(this).data("controls") : false,
+            'pager': $(this).attr("data-pager") ? $(this).data("pager") : false,
+            'responsive': $(this).attr("data-responsive") ? $(this).data("responsive") : false,
+            'dimensions': $(this).attr("data-dimensions") ? $(this).data("dimensions") : "100,100",
+            'increase': $(this).attr("data-increase") ? $(this).data("increase") : false,
+            'pauseOnHover': $(this).attr("data-pauseOnHover") ? $(this).data("pauseOnHover") : false,
+            'slideEndAnimation': true,
+            'autoChange': $(this).attr("data-autoChange") ? $(this).data("autoChange") : false,
+            'pauseOnHover': $(this).attr("data-pauseOnHover") ? $(this).data("pauseOnHover") : false
+        });
+    });
+});
