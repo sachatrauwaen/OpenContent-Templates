eebb63cc96dc318bbad1386468cf00f84ce64545
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2018-12-03 16:59:14 +0100

    classifields

---
+++ Classifieds/template.js
@@ -0,0 +1,18 @@
+(function ($) {
+    $(document).ready(function () {
+        initPage(document);
+    });
+    $(document).on("opencontent.change", function (event, element) {
+        initPage(element);
+    });
+    function initPage(element) {
+        $(".jplist", element).each(function () {
+            $(this).jplist({
+                itemsBox: '.list', 
+				itemPath: '.list-item', 
+				panelPath: '.jplist-panel',
+				deepLinking: true    
+            });
+        });
+    }
+}(jQuery));
